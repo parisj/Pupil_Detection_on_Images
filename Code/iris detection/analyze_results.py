025b5b04f7069107a2468ffef6b8fc4c293b1c54
Anton Paris <anton.j.paris@gmail.com>
2023-04-26 14:03:21 +0200

    working on plot layout and information

--- Code/iris detection/analyze_results.py
+++ Code/iris detection/analyze_results.py
@@ -1,22 +1,78 @@
-import math 
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 from scipy.stats import zscore
 import numpy as np 
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
 
-def analyze_dataframe(path, column, x_error_column, y_error_column):
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Soure: https://matplotlib.org/stable/gallery/statistics/confidence_ellipse.html
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensional dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the standard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the standard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+
+def analyze_dataframe(path,name_file, column, x_error_column, y_error_column):
     # Load the saved dataframe
     df = pd.read_excel(path, sheet_name='Sheet1', index_col=None, header=0)
     print(df)
-    
+    name = 'File: ' + name_file
     # Calculate the average mean of the chosen column
     mean = df[column].mean()
     
     # Identify outliers and calculate the z-score for each
     z_scores = zscore(df[column])
-    threshold = 1
+    threshold = 3
     outliers = np.where(np.abs(z_scores) > threshold)[0]
     num_outliers = len(outliers)
     
@@ -28,49 +84,66 @@ def analyze_dataframe(path, column, x_error_column, y_error_column):
         'Z-Scores': [z_scores]
     })
     
-    sns.set_theme(style="darkgrid")
-    sns.set_style("ticks")
 
 
     euclidean_distance = np.linalg.norm(df[['x_error', 'y_error']], axis=1)
     filtered_df = df[euclidean_distance < 20]
-    x_min, x_max = np.percentile(filtered_df['x_error'], [0, 100])
-    y_min, y_max = np.percentile(filtered_df['y_error'], [0, 100]) 
-    
-    
-    
-    #f, ax = plt.subplots(figsize=(6, 6))
-    #sns.scatterplot(data= filtered_df, x= y_error_column, y=x_error_column, s=5, color=".15")
-    #sns.histplot(data= filtered_df,x=y_error_column, y=x_error_column, bins=50, pthresh=.1, cmap="mako")
-    #sns.kdeplot(data= filtered_df,x= y_error_column, y=x_error_column, levels=5, color="w", linewidths=1)
-    
+    x_min, x_max = np.percentile(filtered_df['x_error'], [2.5, 97.5])
+    y_min, y_max = np.percentile(filtered_df['y_error'], [2.5, 97.5]) 
+    min_limit = min(x_min,y_min)
+    max_limit = max(x_max,y_max)
+    array_error = filtered_df[['x_error', 'y_error']].to_numpy()
+
     # Create plot
     cmap = sns.cubehelix_palette(rot=-.2,light=1, as_cmap=True)
+    fig, ax = plt.subplots(figsize=(6, 6))
+    plt.grid(True, which='major', axis='both')
+
     ax = sns.kdeplot(data=df, x=x_error_column, y=y_error_column, 
-                cmap=cmap, fill=True, clip=(-25, 25), cut=5,
+                cmap=cmap, fill=True, clip=(-25, 25), cut=0,
                 thresh=0, levels=20
                )
-    plt.grid(True, linestyle='--', color='grey', alpha=0.5)
-    plt.xticks(np.linspace(x_min, x_max, 5))
+
+    sns.set_theme()
+    sns.despine(left=True, bottom=True)
+    plt.xticks(range(round(min_limit)-2, round(max_limit)+2, 1))
     plt.xlabel('X Error')
-    plt.yticks(np.linspace(y_min, y_max, 5))
+    plt.yticks(range(round(min_limit)-2, round(max_limit)+2, 1))
     plt.ylabel('Y Error')
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
 
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
-
+    plt.xlim(x_min-0.5, x_max+0.5)
+    plt.ylim(y_min-0.5, y_max+0.5)
+    general_info = '\n'.join((
+        r'z-score threshold: $%.2f$' % (threshold, ),
+        r'Number of outliers: $%d$' % (num_outliers, ),
+        r'Mean: $%.2f$' % (mean, )))
     
+    props = dict(boxstyle='square', facecolor='white', alpha=0.7,edgecolor='gray')
+    
+    ax.text(0.95, 0.95, general_info, transform=ax.transAxes, fontsize=10,
+            verticalalignment='top',horizontalalignment='right', bbox=props)
+    
+    ax.text(0.95,0.80, name, transform=ax.transAxes, fontsize=8, 
+            verticalalignment='top',horizontalalignment='right', bbox=props)
+
+    confidence_ellipse(array_error[:,0], array_error[:,1], ax, n_std=1,
+                       label=r'$1\sigma$', edgecolor='black')
+    
+    ax.grid(color='gray', linestyle='--', linewidth=0.5, alpha=0.5)
+    ax.legend(loc= 'upper left', fontsize=10)
+    ax.set_title('Error Distribution in X and Y Axis')
     plt.tight_layout()
-#
-    plt.show()
+    
+    plt.savefig('Code/iris detection/results/figures/'+name_file+'.png', dpi=300)
     #
     # Return the result dataframe
     return result_df
 
+
+def analyse(path):
+    name = path.split('/')[-1].split('.')[0]
+    analyze_dataframe(path, name,'euclidean distance label - measured', 'x_error', 'y_error')
+
 if __name__ == '__main__':
     path= 'Code/iris detection/results/LPW_1_4.xlsx'
-    analyze_dataframe(path, 'euclidean distance label - measured', 'x_error', 'y_error')
+    analyse(path)
